53e839222f204bbf3aef2104bce8065d09dc69b2
Elita Chan <elitachan03@gmail.com>
2023-01-25 16:47:34 -0500

    Include cases for 1 num for + - * /

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function add(num1, num2) {
-    return num1 + num2;
+    return num2 ? num1 + num2 : num1 + num1;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return num2 ? num1 - num2 : 0;
 }
 function multiply(num1, num2) {
-    return num1 * num2;
+    return num2 ? num1 * num2 : num1 * num1;
 }
 
 function divison(num1, num2) {
-    return num1 / num2;
+    return num2 ? num1 / num2 : 1;
 }
 
 function percent(num) {
