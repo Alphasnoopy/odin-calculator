ed39b03a05f4cd27d44c9f50b73d5b11db21f8e8
Elita Chan <elitachan03@gmail.com>
2023-03-22 20:37:08 -0400

    Reformat entire code
    Reads from top to bottom, follow ti-108 guidebook for computations,
    limit amount of digits and provide E sign when necessary

--- script.js
+++ script.js
@@ -1,44 +1,350 @@
-const enter = document.getElementById('enter');
-const operators = document.querySelectorAll('.operator');
-const operatorOthers = document.querySelectorAll('.operatorOther');
-const numericals = document.querySelectorAll('.num');
 const display = document.getElementById('displayNum');
-const onClear = document.getElementById('onClear');
-const sideDesigns = document.querySelectorAll('.sideDesign');
-const btnDesigns = document.querySelectorAll('.btnDesign');
-const decimal = document.getElementById('decimal');
-const negBtn = document.getElementById('neg');
-const negSign = document.getElementById('negSign');
-const mrc = document.getElementById('mrc');
-const mMinus = document.getElementById('mMinus');
-const mAdd = document.getElementById('mAdd');
+const dot = document.getElementById('dot');
 const mSign = document.getElementById('mSign');
+const negSign = document.getElementById('negSign');
+const eSign = document.getElementById('eSign');
 
-let operatorList = [];
-let numList = ['',''];
 let start = false;
-let memory = '0';
-let mClickOnce = false;
+let clearOnce = false;
+let mOnce = false;
+let currNum = '';
+let currOp = '';
+let constantNum = '';
+let constantOp = '';
+let numList = [];
+let memory = '';
+const ogColor = 'rgba(49, 53, 49, 0.295)';
 
-function checkListSize(amtRepeat, numRepeat) {
-    for (let i=0; i <= amtRepeat; i++) {
-        numList.splice(1, 0, numRepeat);
-    };
+main();
+
+function main() {
+    const allBtns = document.getElementsByTagName('button');
+
+    Array.from(allBtns).forEach(btn => btn.addEventListener('click', () => calculator(btn)));
 }
 
-function add(num1, num2) {
-    return num2 ? num1 + num2 : num1 + num1;
+function calculator(btnElement) {
+    let btnContent = '';
+    const btnID = btnElement.id;
+    const btnCategory = (btnElement.classList.length !== 0)? btnElement.className : btnID;
+
+    switch(btnCategory) {
+        case ('onClear') :
+            start = true;
+            mOnce = false;
+            display.parentNode.style.color = 'black';
+            if (!clearOnce) {
+                currNum = '0';
+                displayStyle(currNum);
+                clearOnce = true;
+            }
+            else {
+                reset();
+                clearOnce = false;
+            }
+            break;
+        case ('num') :
+            start = true;
+            display.parentNode.style.color = 'black';
+            if (currNum.replace(/[-.]/g, "").length < 8 && typeof currNum === 'string') {
+                if (mOnce) {
+                    currNum = '';
+                    mOnce = false;
+                }
+                btnContent = btnElement.firstChild.textContent;
+
+                (currNum === '0')? currNum = btnContent : currNum += btnContent;
+                displayStyle(currNum);
+
+                console.log(numList);
+                console.log(currNum);
+            }
+            break;
+        case ('decimal') :
+            start = true;
+            display.parentNode.style.color = 'black';
+            if (mOnce) {
+                currNum = '0.';
+                mOnce = false;
+            }
+            else if (!currNum.includes('.')) {
+                if (currNum === '') currNum = '0';
+                currNum += '.';
+            }
+            displayStyle(currNum);
+            break;
+        case ('neg') :
+            if (start) {
+                mOnce = false;
+                let negated = 0;
+                if (currNum !== '') {
+                    negated = currNum * -1;
+                    currNum = String(negated);
+                    displayStyle(currNum);
+                }
+                else {
+                    numList[0] = numList[0] * -1;
+                    displayStyle(numList);
+                }
+            }
+            break;
+        case ('operator') :
+            if (start) {
+                mOnce = false;
+                constantNum = '';
+                constantOp = '';
+
+                if (currNum !== '') {
+                    if (numList.length !== 0) {
+                        numList[0] = currNum;
+                    }
+                    else {
+                        numList.push(currNum);
+                    }
+                    currNum = '';
+                }
+
+                if (numList.length === 2) {
+                    evaluate();
+                    displayStyle(numList[0]);
+                }
+                currOp = btnID;
+            }
+            break;
+        case ('sqrt') :
+            if (start) {
+                mOnce = false;
+                if (currNum === '') {
+                    numList[0] = sqrt(parseFloat(numList[0]));
+                    displayStyle(numList[0]);
+                }
+                else {
+                    currNum = sqrt(parseFloat(currNum));
+                    displayStyle(currNum);
+                }
+            }
+            break;
+        case ('percent') :
+            if (start) {
+                mOnce = false;
+                constantNum = '';
+                constantOp = '';
+
+                if (currNum === '' && currOp === '') {
+                    numList[0] = percent(parseFloat(numList[0]));
+                }
+                else if (currNum === '') {
+                    numList.push(numList[0]);
+                    evaluate();
+                    numList[0] = percent(parseFloat(numList[0]));
+                }
+                else {
+                    if (currOp === 'add' || currOp === 'subtract') {
+                        numList.push(multiply(numList[0], percent(parseFloat(currNum))));
+                    }
+                    else {
+                        numList.push(percent(parseFloat(currNum)));
+                    }
+                    currNum = '';
+                }
+
+                if (numList.length === 2) {
+                    evaluate();
+                }
+
+                displayStyle(numList[0]);
+            }
+            break;
+        case ('enter') :
+            if (start) {
+                mOnce = false;
+                if (currNum !== '' && constantNum !== '') {
+                    console.log(constantNum);
+                    console.log('enter here');
+                    numList[0] = currNum;
+                    numList.push(constantNum);
+                    currOp = constantOp;
+                    evaluate();
+                }
+                else if (currOp === '') {
+                    console.log(constantNum);
+                    console.log('enter here 2');
+                    numList.push(currNum);
+                }
+                else if (currNum === '' && constantNum !== '') {
+                    numList.push(constantNum);
+                    currOp = constantOp;
+                    evaluate();
+                }
+                else if (currNum === '') {
+                    console.log('enter here 4');
+                    numList.push(numList[0]);
+                    evaluate();
+                }
+                else {  
+                    numList.push(currNum);
+                    evaluate();
+                }
+
+                if (constantNum === '' && currOp !== '') {
+                    console.log('constantNum');
+                    (currOp === 'multiply')? constantNum = numList[0] : constantNum = currNum;
+                    constantOp = currOp;
+                }
+
+                currNum = '';
+                currOp = '';
+                displayStyle(numList[0]);
+            }
+            break;
+        case ('mrc') :
+            if (start) {
+                if (!mOnce) {
+                    displayStyle(memory);
+                    mSign.style.color = 'black';
+                    currNum = String(memory);
+                    mOnce = true;
+                }
+                else {
+                    memory = 0;
+                    mSign.style.color = ogColor;
+                    mOnce = false;
+                }
+            }
+            break;
+        case ('mMinus') :
+            if (start) {
+                mOnce = false;
+                if (currNum === '' && currOp !== '') {
+                    numList.push(numList[0]);
+                    evaluate();
+                    tempNum = numList[0];
+                }
+                else if (numList.length === 0) {
+                    tempNum = currNum;
+                    currNum = '';
+                }
+                else if (currNum !== '') {
+                    numList.push(currNum);
+                    currNum = '';
+                    evaluate();
+                    tempNum = numList[0];
+                }
+                else {
+                    tempNum = numList[0]
+                }
+                mSign.style.color = 'black';
+                displayStyle(tempNum); 
+                memory = memory - tempNum; 
+            }
+            break;
+        case ('mAdd') :
+            if (start) {
+                mOnce = false;
+                if (currNum === '' && currOp !== '') {
+                    numList.push(numList[0]);
+                    evaluate();
+                    tempNum = numList[0];
+                }
+                else if (numList.length === 0) {
+                    tempNum = currNum;
+                    currNum = '';
+                }
+                else if (currNum !== '') {
+                    numList.push(currNum);
+                    currNum = '';
+                    evaluate();
+                    tempNum = numList[0];
+                }
+                else {
+                    tempNum = numList[0]
+                }
+                mSign.style.color = 'black';
+                displayStyle(tempNum); 
+                memory = memory + tempNum; 
+            }
+            break;
+    }
 }
 
-function subtract(num1, num2 = num1) {
-    return num2 ? num1 - num2 : num1 - num1;
+function reset() {
+    currNum = '';
+    numList = [];
+    currOp = '';
+    constantOp = '';
+    constantNum = '';
+    negSign.style.color = ogColor;
+    eSign.style.color = ogColor;
 }
-function multiply(num1, num2 = num1) {
-    return num2 ? num1 * num2 : num1 * num1;
+
+function displayDot(num) {
+    (String(num).includes('.')) ? dot.style.display = 'none' : dot.style.display = 'inline';
+}
+
+function displayNeg(num) {
+    (String(num)[0] === '-') ? negSign.style.color = 'Black' : negSign.style.color = ogColor;
+}
+
+function displayE(num) {
+    let splitNum = String(num).replace(/[-]/g,"").split('.');
+    console.log(splitNum);
+    if (String(num).replace(/[-.]/g,"").length > 8) {
+        (splitNum[0].length > 8) ? eSign.style.color = 'Black' : eSign.style.color = ogColor;
+    }
+    if (String(num)[0] === '-') {
+        num = String(num).replace('-','');
+    }
+    (String(num).includes('.')) ? display.textContent = String(num).slice(0,9) : display.textContent = String(num).slice(0,8);
+}
+
+function displayStyle(num) {
+    displayDot(num);
+    displayNeg(num);
+    displayE(num);
+}
+
+function evaluate() {
+    let result = 0;
+    numList = numList.reverse();
+    console.log('here');
+    console.log(numList);
+    num1 = parseFloat(numList.pop());
+    num2 = parseFloat(numList.pop());
+    
+    switch(currOp) {
+        case 'divide' :
+            result = divide(num1, num2);
+            numList.push(result);
+            break;
+        case 'multiply' :
+            result = multiply(num1, num2);
+            numList.push(result);
+            break;
+        case 'subtract' :
+            result = subtract(num1, num2);
+            numList.push(result);
+            break;
+        case 'add' :
+            result = add(num1, num2);
+            numList.push(result);
+            break;   
+    }
 }
 
 function divide(num1, num2) {
-    return num2 ? num1 / num2 : num1 / num1;
+    return num1 / num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
 }
 
 function percent(num) {
@@ -48,216 +354,3 @@ function percent(num) {
 function sqrt(num) {
     return Math.sqrt(num);
 }
-
-function operate() {
-    switch (operatorList.pop()) {
-        case 'add':
-            numList.push(add(numList.pop(), numList.pop()));
-            if (operatorList) {
-                operate();
-            };
-            break;
-        case 'subtract':
-            numList.push(subtract(numList.pop(), numList.pop()));
-            if (operatorList) {
-                operate();
-            };
-            break;
-        case 'multiply':
-            numList.push(multiply(numList.pop(), numList.pop()));
-            if (operatorList) {
-                operate();
-            };
-            break;
-        case 'divide':
-            numList.push(divide(numList.pop(), numList.pop()));
-            if (operatorList) {
-                operate();
-            };
-            break;
-    }
-}
-
-function operateSteps(amtRepeat, numRepeat) {
-    numList = numList.filter((num) => num !== '');
-    checkListSize(amtRepeat, numRepeat);
-    operatorList.reverse();
-    numList = numList.reverse();
-    numList = numList.map((num) => {return parseFloat(num)});
-    operate();
-    numList.push('');
-}
-
-function displayNum(num) {
-    (String(num).includes('.')) ? decimal.style.display = 'none' : decimal.style.display = 'inline';
-    display.textContent = String(num).replace('-','');
-}
-
-function checkNeg(num) {
-    (String(num).includes('-')) ? negSign.style.color = 'Black' : negSign.style.color = 'rgba(49, 53, 49, 0.295)';
-}
-
-function operateOther(operator) {
-    let sqrtnum = 0;
-    // +1 because yet to filter ''
-    checkListSize(operatorList.length-numList.length+1, numList[0]);
-
-    switch (operator) {
-        case 'percent':
-            // same case pop single number
-            numList = numList.filter((num) => num !== '');
-            numList.push(percent(parseFloat(numList.pop())));
-            if (operatorList) {
-                operateSteps(operatorList.length-numList.length, numList[0]);
-            }
-            displayNum(numList[0]);
-            checkNeg(numList[0]);
-            break;
-        case 'sqrt':
-            // pop doesnt work for single number
-            if (numList.includes('')) {
-                sqrtnum = sqrt(parseFloat(numList[0]));
-                numList.splice(-2, 1, sqrtnum);
-            }
-            else {
-                sqrtnum = sqrt(parseFloat(numList.pop()));
-                numList.push(sqrtnum);
-            }
-            displayNum(sqrtnum);
-            break;
-    }
-}
-
-function calculates() {
-    sideDesigns.forEach((side) => {
-        for(let i = 0; i < 80; i++) {
-            let div = document.createElement('div');
-            div.classList.add('sideBox');
-            side.appendChild(div);
-        }
-    })
-
-    btnDesigns.forEach((btn) => {
-        let currClass = '';
-        btn.parentNode.classList.contains('num') ? currClass = 'btnBoxNum' : currClass = 'btnBox';
-        for(let i = 0; i < 27; i++) {
-            let div = document.createElement('div');
-            div.classList.add(currClass);
-            btn.appendChild(div);
-        }
-    })
-
-    operators.forEach((operator) => {operator.addEventListener('click', () => { 
-        if (start) {
-            if (!numList.includes('')) {
-                operateSteps(operatorList.length-numList.length, numList[0]);
-                displayNum(numList[0]);
-                checkNeg(numList[0]);
-            }
-            operatorList.push(operator.id);
-        }
-    })});
-    
-    operatorOthers.forEach((operator) => {operator.addEventListener('click', () => {operateOther(operator.id)})});
-    enter.addEventListener('click', () => {
-        if (start) {
-            console.log(operatorList);
-        if (operatorList) {
-            operateSteps(operatorList.length-numList.length, numList[0]);
-        }
-        displayNum(numList[0]);
-        checkNeg(numList[0]);
-        }
-    });
-
-    negBtn.addEventListener('click', () => {
-        if (start) {
-            let negated = 0;
-        if (numList[numList.length-1] !== '') {
-            negated = numList.pop() * -1;
-            numList.push(String(negated));
-        }
-        else {
-            negated = numList[0] * -1;
-            numList[0] = String(negated);
-        }
-        checkNeg(negated);
-        }
-    });
-
-    mrc.addEventListener('click', () => {
-        if (start) {
-            if (mClickOnce === false) {
-                displayNum(memory);
-                checkNeg(memory);
-                mSign.style.color = 'Black';
-                mClickOnce = true;
-            }
-            else {
-                displayNum(memory);
-                checkNeg(memory);
-                numList[0] = parseFloat(memory);
-                memory = '0';
-                mSign.style.color = 'rgba(49, 53, 49, 0.295)';
-                mClickOnce = false;
-            }
-        }
-    });
-
-    mAdd.addEventListener('click', () => {
-        if (start) {
-            operateSteps(operatorList.length-numList.length, numList[0]);
-            memory = parseFloat(memory) + parseFloat(numList[0]);
-            displayNum(numList[0]);
-            checkNeg(numList[0]);
-            mSign.style.color = 'Black';
-        }
-    });
-
-    mMinus.addEventListener('click', () => {
-        if (start) {
-            operateSteps(operatorList.length-numList.length, numList[0]);
-            memory = parseFloat(memory) - parseFloat(numList[0]);
-            displayNum(numList[0]);
-            checkNeg(numList[0]);
-            mSign.style.color = 'Black';
-        }
-    });
-
-    numericals.forEach((number) => {number.addEventListener('click', () => {
-        display.parentNode.style.color = 'Black';
-        start = true;
-        if(numList[0] === '0') {
-            numList[0] = '';
-        }
-        if (operatorList.length > 0) {           
-            if (operatorList.length-numList.length === -2) {
-                numList.splice(-2, 1);
-            }
-            numList[numList.length-1] += number.firstChild.textContent;
-            displayNum(numList[numList.length-1]);
-            checkNeg(numList[numList.length-1]);
-        }
-        else if (typeof numList[0] !== 'string'){
-            numList[0] = number.firstChild.textContent;
-            displayNum(numList[0]);
-            checkNeg(numList[0]);
-            }
-        else {
-            numList[0] += number.firstChild.textContent;
-            displayNum(numList[0]);
-            checkNeg(numList[0]);
-        }
-    })});
-
-    onClear.addEventListener('click', () => {
-        display.parentNode.style.color = 'Black';
-        start = true;
-        operatorList = [];
-        numList = ['0',''];
-        displayNum(numList[0]);
-    });
-    
-}
-
-calculates()    
